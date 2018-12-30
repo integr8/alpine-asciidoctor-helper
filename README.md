71cb68d18bf454a8b345b7944e3a21a5852b4c5a
Fábio Luciano <fabio@naoimporta.com>
2018-12-30 14:52:54 -0200

    Resolvendo problema do output

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # alpine-asciidoctor-helper
+
+[![Build Status](https://travis-ci.org/integr8/alpine-asciidoctor-helper.svg?branch=development)](https://travis-ci.org/integr8/alpine-asciidoctor-helper)
+
+Documentação: https://integr8.github.io/alpine-asciidoctor-helper/
--- files/plugin-helper.sh
+++ files/plugin-helper.sh
@@ -1,8 +1,10 @@
-ls -l *.rb
+export ASCIIDOCTOR_PLUGIN_OPTIONS=''
 
 if [[ ! -z "${ASCIIDOCTOR_PLUGIN}" ]]; then
   IFS=',' read -ra PLUGINS <<< "${ASCIIDOCTOR_PLUGIN}"
-  for plugin in "${PLUGINS[@]}"; do
-      echo $plugin
+  for PLUGIN in "${PLUGINS[@]}"; do
+      if [[ -f "$ASCIIDOCTOR_PLUGIN_PATH/lib/$PLUGIN.rb" ]]; then
+        ASCIIDOCTOR_PLUGIN_OPTIONS="${ASCIIDOCTOR_PLUGIN_OPTIONS} -r ${ASCIIDOCTOR_PLUGIN_PATH}/lib/${PLUGIN}.rb"
+      fi
   done
 fi
