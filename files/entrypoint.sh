a5ce10ad665fc2ba56105fd7133b919f38d905a1
Fábio Luciano <fabio@naoimporta.com>
2019-02-08 18:27:57 -0200

    Modificando estrutura

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 export ASCIIDOCTOR_HTML_THEME_PATH='/opt/asciidoctor/themes'
 export ASCIIDOCTOR_PLUGIN_PATH='/opt/asciidoctor/plugins'
-export ASCIIDOCTOR_OUTPUT_PATH='/documents/output'
+export ASCIIDOCTOR_OUTPUT_PATH='/documents'
 
 mkdir -p $ASCIIDOCTOR_OUTPUT_PATH
 
